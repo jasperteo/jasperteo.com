2d4f2c3f45ab6ec1bb8373daebbbf2f1695a0b89
jasperteo <jaspertzj@outlook.sg>
2024-05-09 18:27:51 +0800

    install cloudflare adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import UnoCSS from "unocss/astro";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), UnoCSS({ injectReset: true })],
   site: "https://jasperteo.com/",
   prefetch: { prefetchAll: true },
-  output: "static",
+  output: "hybrid",
+  adapter: cloudflare({
+    platformProxy: { enabled: true },
+    imageService: "passthrough",
+  }),
   vite: {
     css: { transformer: "lightningcss" },
     build: { cssMinify: "lightningcss" },
