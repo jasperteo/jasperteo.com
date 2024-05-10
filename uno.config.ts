b4bcf980f68f812c409ec1d13851aeb6d4dd0c2f
jasperteo <jaspertzj@outlook.sg>
2024-05-11 00:51:08 +0800

    shift fonts

--- uno.config.ts
+++ uno.config.ts
@@ -1,28 +1,18 @@
-import { defineConfig, presetUno, presetWebFonts } from "unocss";
+import { defineConfig, presetUno } from "unocss";
 import presetAnimations from "unocss-preset-animations";
 import { presetShadcn } from "unocss-preset-shadcn";
 
 export default defineConfig({
   presets: [
     presetUno({ dark: "class" }),
-    presetWebFonts({
-      provider: "google",
-      fonts: {
-        sans: "IBM Plex Sans:400,500,600,700,800,900",
-        mono: "Sono:400,500,600,700,800,900",
-        welcome: [
-          {
-            name: "Playpen Sans",
-            weights: [`400`, `500`, `600`, `700`, `800`, `900`],
-          },
-          {
-            name: "ZCOOL KuaiLe",
-            weights: [`400`],
-          },
-        ],
-      },
-    }),
     presetShadcn({ color: "orange", radius: 0.75 }),
     presetAnimations(),
   ],
+  theme: {
+    fontFamily: {
+      sans: "IBM Plex Sans, ui-sans-serif, system-ui, sans-serif",
+      mono: "Sono, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, monospace",
+      welcome: "Playpen Sans, ZCOOL KuaiLe, sans-serif",
+    },
+  },
 });
