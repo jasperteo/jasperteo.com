ca7af31169448510af0ab5a468aba4c4565e20e3
jasperteo <jaspertzj@outlook.sg>
2024-05-29 22:12:06 +0800

    trying vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
 import UnoCSS from "unocss/astro";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), UnoCSS({ injectReset: true })],
   site: "https://jasperteo.com/",
   output: "server",
-  adapter: cloudflare({
-    platformProxy: { enabled: true },
-    imageService: "passthrough",
-  }),
+  adapter: vercel(),
+  // adapter: cloudflare({
+  //   platformProxy: { enabled: true },
+  //   imageService: "passthrough",
+  // }),
   vite: {
     css: { transformer: "lightningcss" },
     build: { cssMinify: "lightningcss" },
