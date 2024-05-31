8e27d8b12f435bc844dfba2793bd3a7e7a4df108
jasperteo <jaspertzj@outlook.sg>
2024-06-01 00:47:24 +0800

    update

--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -1,13 +1,22 @@
-import { Match, Switch, createEffect, createSignal, onCleanup } from "solid-js";
+import {
+  Match,
+  Switch,
+  createEffect,
+  createSignal,
+  onCleanup,
+  onMount,
+} from "solid-js";
 import Cookies from "js-cookie";
 import { Icon } from "@iconify-icon/solid";
 
 import { Button } from "@/components/ui/button";
 
-const storageKey = "theme";
-const themeValues = ["dark", "light", "system"] as const;
+const themeValues = ["light", "dark", "system"] as const;
 type Theme = (typeof themeValues)[number];
 const defaultTheme: Theme = "dark";
+
+const storageKey = "theme";
+const color = { light: "hsl(0, 0%, 98%)", dark: "hsl(240, 6%, 10%)" };
 
 type DarkModeToggleProps = { storedTheme?: Theme };
 
@@ -15,22 +24,18 @@ const DarkModeToggle = (props: DarkModeToggleProps) => {
   const storedTheme = () => props.storedTheme;
   const [theme, setTheme] = createSignal<Theme>(storedTheme() ?? defaultTheme);
 
+  const themeColor = () => (theme() === "light" ? color.light : color.dark);
+
   const prefersLight = window.matchMedia("(prefers-color-scheme: light)");
   const setMediaTheme = () => setTheme(prefersLight.matches ? "light" : "dark");
-
-  prefersLight.addEventListener("change", setMediaTheme);
-  onCleanup(() => prefersLight.removeEventListener("change", setMediaTheme));
 
   createEffect(() => {
     if (theme() !== "light" && theme() !== "dark") {
       setMediaTheme();
     }
-
-    const themeColor =
-      theme() === "light" ? "hsl(0, 0%, 98%)" : "hsl(240, 6%, 10%)";
     document.documentElement.classList.remove(...themeValues);
     document.documentElement.classList.add(theme());
-    document.getElementById("theme-color")?.setAttribute("content", themeColor);
+    document.getElementById("tc")?.setAttribute("content", themeColor());
     Cookies.set(storageKey, theme(), {
       expires: 365,
       sameSite: "strict",
@@ -38,13 +43,16 @@ const DarkModeToggle = (props: DarkModeToggleProps) => {
     });
   });
 
+  onMount(() => prefersLight.addEventListener("change", setMediaTheme));
+  onCleanup(() => prefersLight.removeEventListener("change", setMediaTheme));
+
   return (
     <Button
-      class="text-lg"
-      aria-label="Toggle dark mode"
+      aria-label="Dark Mode Toggle"
       variant="outline"
       size="icon"
-      onMouseDown={[setTheme, theme() === "dark" ? "light" : "dark"]}
+      class="text-lg"
+      onClick={[setTheme, theme() === "dark" ? "light" : "dark"]}
     >
       <Switch fallback={<Icon icon="line-md:light-dark-loop" />}>
         <Match when={theme() === "dark"}>
@@ -58,5 +66,5 @@ const DarkModeToggle = (props: DarkModeToggleProps) => {
   );
 };
 
-export { DarkModeToggle, storageKey, themeValues, defaultTheme };
+export { DarkModeToggle, color, defaultTheme, storageKey };
 export type { Theme };
