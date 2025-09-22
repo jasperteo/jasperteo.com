1c69eb061156b542a6b925e8b68cfa04b752a0dd
Jasper 張 <jaspertzj@outlook.sg>
2025-09-23 01:31:20 +0800

    Update Devtools component to conditionally render based on production environment

--- src/components/devtools.tsx
+++ src/components/devtools.tsx
@@ -1,10 +1,15 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
+/**
+ * @todo (@jasperteo): remove this when Cloudflare can build properly without this flag
+ */
+const isProduction = import.meta.env.PROD;
+
 type DevtoolsProps = { enabled?: boolean };
 
 function Devtools({ enabled = false }: DevtoolsProps) {
-	if (!enabled) {
+	if (!enabled || isProduction) {
 		// oxlint-disable-next-line no-null
 		return null;
 	}
