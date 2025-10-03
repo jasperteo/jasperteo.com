48ed1c9b948760ea4902190baf30eeb19af4668c
Jasper 張 <jaspertzj@outlook.sg>
2025-10-03 19:58:31 +0800

    Refactor Devtools component and remove unused devtools plugin from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
-import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { fontless } from "fontless";
@@ -8,7 +7,6 @@ import { defineConfig } from "vite";
 
 const viteConfig = defineConfig({
 	plugins: [
-		devtools(),
 		tanstackStart({
 			prerender: { enabled: true, crawlLinks: true },
 			sitemap: { enabled: true, host: "https://jasperteo.com" },
@@ -19,11 +17,6 @@ const viteConfig = defineConfig({
 		tailwindcss(),
 	],
 	resolve: { alias: { "@": "/src" } },
-	/* @todo (@jasperteo): remove this when Cloudflare can build properly without this flag */
-	define: {
-		"globalThis.Cloudflare.compatibilityFlags":
-			"{enable_nodejs_process_v2: true}",
-	},
 });
 
 export default viteConfig;
