83109504d285604e07180b6550118546b7cb8de3
Jasper 張 <jaspertzj@outlook.sg>
2025-10-20 17:14:30 +0800

    feat: enhance Description component with motion effects and improve tab layout

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,33 +1,53 @@
 import { Tabs as TabsPrimitive } from "@base-ui-components/react/tabs";
-import type { HTMLMotionProps } from "motion/react";
-import { motion } from "motion/react";
 
 import { cn } from "@/utils/utils";
+
+type TabsVariant = "default" | "underline";
 
 function Tabs({ className, ...props }: TabsPrimitive.Root.Props) {
 	return (
 		<TabsPrimitive.Root
 			data-slot="tabs"
-			className={cn("flex flex-col", className)}
+			className={cn(
+				"flex flex-col gap-2 data-[orientation=vertical]:flex-row",
+				className
+			)}
 			{...props}
 		/>
 	);
 }
 
-function TabsList({ className, children, ...props }: TabsPrimitive.List.Props) {
+type TabsListProps = TabsPrimitive.List.Props & { variant?: TabsVariant };
+
+function TabsList({
+	variant = "default",
+	className,
+	children,
+	...props
+}: TabsListProps) {
 	return (
 		<TabsPrimitive.List
 			data-slot="tabs-list"
 			className={cn(
-				"bg-muted text-muted-foreground relative inset-0 z-0 inline-flex h-9 w-fit items-center justify-center rounded-full p-1",
+				"text-muted-foreground relative z-0 flex w-fit items-center justify-center gap-x-0.5",
+				"data-[orientation=vertical]:flex-col",
+				variant === "default"
+					? "bg-muted text-muted-foreground/64 rounded-full p-1"
+					: "*:data-[slot=tabs-trigger]:hover:bg-accent data-[orientation=horizontal]:py-1 data-[orientation=vertical]:px-1",
 				className
 			)}
 			{...props}
 		>
 			{children}
 			<TabsPrimitive.Indicator
+				data-slot="tab-indicator"
+				className={cn(
+					"absolute bottom-0 left-0 h-(--active-tab-height) w-(--active-tab-width) translate-x-(--active-tab-left) -translate-y-(--active-tab-bottom) transition-[width,translate] duration-300 ease-out",
+					variant === "underline"
+						? "bg-primary z-10 data-[orientation=horizontal]:-bottom-[calc(--spacing(1)+1px)] data-[orientation=horizontal]:h-0.5 data-[orientation=vertical]:-start-[calc(--spacing(1)+1px)] data-[orientation=vertical]:w-0.5"
+						: "bg-background dark:bg-accent -z-1 rounded-full shadow-sm"
+				)}
 				renderBeforeHydration
-				className="bg-background/70 dark:bg-popover dark:border-input absolute top-1/2 left-0 -z-10 h-7 w-(--active-tab-width) translate-x-(--active-tab-left) -translate-y-1/2 rounded-full border border-transparent shadow-sm transition-[width,translate] duration-300 ease-out"
 			/>
 		</TabsPrimitive.List>
 	);
@@ -36,9 +56,12 @@ function TabsList({ className, children, ...props }: TabsPrimitive.List.Props) {
 function TabsTab({ className, ...props }: TabsPrimitive.Tab.Props) {
 	return (
 		<TabsPrimitive.Tab
-			data-slot="tabs-tab"
+			data-slot="tabs-trigger"
 			className={cn(
-				"focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring text-foreground dark:text-muted-foreground dark:data-[selected]:text-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-colors duration-300 ease-out focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+				"focus-visible:ring-ring flex flex-1 shrink-0 items-center justify-center rounded-md border border-transparent text-sm font-medium whitespace-nowrap transition-[color,background-color,box-shadow] outline-none focus-visible:ring-2 data-disabled:pointer-events-none data-disabled:opacity-64 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+				"hover:text-muted-foreground data-selected:text-foreground",
+				"gap-1.5 px-2 py-1",
+				"data-[orientation=vertical]:w-full data-[orientation=vertical]:justify-start",
 				className
 			)}
 			{...props}
@@ -50,26 +73,10 @@ function TabsPanel({ className, ...props }: TabsPrimitive.Panel.Props) {
 	return (
 		<TabsPrimitive.Panel
 			data-slot="tabs-panel"
-			className={cn(
-				"animate-in fade-in blur-in-xs duration-300 ease-out",
-				"flex-1 outline-none",
-				className
-			)}
+			className={cn("flex-1 outline-none", className)}
 			{...props}
 		/>
 	);
 }
 
-function TabsPanelList({ className, ...props }: HTMLMotionProps<"div">) {
-	return (
-		<motion.div
-			data-slot="tabs-panel-list"
-			layout="position"
-			transition={{ layout: { duration: 0.3, ease: "easeOut" } }}
-			className={cn("overflow-clip", className)}
-			{...props}
-		/>
-	);
-}
-
-export { Tabs, TabsList, TabsPanel, TabsPanelList, TabsTab };
+export { Tabs, TabsList, TabsPanel, TabsTab };
