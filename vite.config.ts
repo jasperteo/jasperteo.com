ec7282594532eb25b8879c25c749ffa2d3b92be3
Jasper 張 <jaspertzj@outlook.sg>
2025-10-20 17:13:34 +0800

    refactor: add devtools plugin and enabled React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { fontless } from "fontless";
@@ -7,8 +8,11 @@ import { defineConfig } from "vite";
 
 const viteConfig = defineConfig({
 	plugins: [
+		devtools(),
 		tanstackStart(),
-		viteReact(),
+		viteReact({
+			babel: { plugins: ["babel-plugin-react-compiler"] },
+		}),
 		cloudflare({ viteEnvironment: { name: "ssr" } }),
 		fontless(),
 		tailwindcss(),
