4772c00d61dc9eb12fafd8b5c65bf4ec99de87ab
Jasper 張 <jaspertzj@outlook.sg>
2025-10-25 16:49:25 +0800

    refactor: streamline Devtools component rendering logic

--- src/components/devtools.tsx
+++ src/components/devtools.tsx
@@ -4,20 +4,17 @@ import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 type DevtoolsProps = { enabled?: boolean };
 
 function Devtools({ enabled = false }: DevtoolsProps) {
-	if (!enabled) {
-		// oxlint-disable-next-line no-null
-		return null;
-	}
-
 	return (
-		<TanStackDevtools
-			plugins={[
-				{
-					name: "TanStack Router",
-					render: <TanStackRouterDevtoolsPanel />,
-				},
-			]}
-		/>
+		enabled && (
+			<TanStackDevtools
+				plugins={[
+					{
+						name: "TanStack Router",
+						render: <TanStackRouterDevtoolsPanel />,
+					},
+				]}
+			/>
+		)
 	);
 }
 
