ad6a7ed35cc096a91a4291118f1425cac8293358
Jasper 張 <jaspertzj@outlook.sg>
2026-05-04 12:46:00 +0800

    feat: enable React Server Components support

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import rsc from "@vitejs/plugin-rsc";
 import { fontless } from "fontless";
 import { defineConfig } from "vite";
 
@@ -11,14 +12,13 @@ const viteConfig = defineConfig({
 	plugins: [
 		devtools(),
 		tanstackStart({
-			prerender: {
-				enabled: true,
-				failOnError: false,
-			},
+			rsc: { enabled: true },
+			prerender: { enabled: true, failOnError: false },
 		}),
+		rsc(),
 		viteReact(),
 		babel({ presets: [reactCompilerPreset()] }),
-		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		cloudflare({ viteEnvironment: { name: "ssr", childEnvironments: ["rsc"] } }),
 		fontless(),
 		tailwindcss(),
 	],
